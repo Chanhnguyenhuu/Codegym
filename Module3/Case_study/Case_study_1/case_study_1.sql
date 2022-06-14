1f466f434741dc6e7a3f713f80b97878d3e7813f
chanhnguyenhuu <nguyenhuuchanh993@gmail.com>
2022-06-14 12:06:31 +0700

    Baitap

--- Module3/Case_study/Case_study_1/case_study_1.sql
+++ Module3/Case_study/Case_study_1/case_study_1.sql
@@ -354,6 +354,54 @@ ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_l
 tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 
 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
 */
+select 	hop_dong.ma_hop_dong,
+		nhan_vien.ho_ten as ho_ten_nhan_vien,
+        khach_hang.ho_ten as ho_ten_khach_hang,
+        khach_hang.so_dien_thoai as sdt_khach_hang,
+        dich_vu.ten_dich_vu,
+        count(hop_dong_chi_tiet.ma_dich_vu_di_kem) as so_luong_dich_vu_di_kem,
+        hop_dong.tien_dat_coc
+from dich_vu
+join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+join nhan_vien on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
+join khach_hang on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+where (hop_dong.ngay_lam_hop_dong between '2020-10-01' and '2020-12-31')
+		and dich_vu.ma_dich_vu not in (select ma_dich_vu
+									from hop_dong
+                                    where hop_dong.ngay_lam_hop_dong between '2021-01-01' and '2021-06-30'
+                                    group by ma_dich_vu
+									);
+                                    
+/*Task 13: Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
+(Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+*/
+select ma_dich_vu_di_kem, sum(so_luong) as so_lan_su_dung
+from hop_dong_chi_tiet
+group by ma_dich_vu_di_kem
+order by so_lan_su_dung desc;
+
+/*Task 14: Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
+Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung 
+(được tính dựa trên việc count các ma_dich_vu_di_kem).
+*/
+select hop_dong_chi_tiet.ma_dich_vu_di_kem
+from dich_vu_di_kem
+join hop_dong_chi_tiet on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+group by hop_dong_chi_tiet.ma_dich_vu_di_kem
+having count(hop_dong_chi_tiet.ma_dich_vu_di_kem) = 1;
+
+select dich_vu_di_kem.ma_dich_vu_di_kem, 
+		dich_vu_di_kem.ten_dich_vu_di_kem,
+        hop_dong.ma_hop_dong
+from dich_vu_di_kem
+join hop_dong on hop_dong.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+where dich_vu_di_kem.ma_dich_vu_di_kem in (select ma_dich_vu_di_kem
+							from hop_dong_chi_tiet
+                            group by ma_dich_vu_di_kem
+                            having count(ma_dich_vu_di_kem) = 1
+                            )
+
 
 
 
